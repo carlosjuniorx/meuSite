3af231fa2726276d56de62ee39e50592a5ecc704
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2021-08-15 09:39:19 -0300

    resolvido problema do build

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,6 @@
 
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+
 
   
 
@@ -14,13 +14,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
         </Head>
         <body>
           <Main />
-          <Script
-              src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"/>
+          
 
           <NextScript>
           {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> */}
