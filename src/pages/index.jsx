e819c95df8d61fed1d119ec7c7095ee57212e1c6
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2021-09-21 18:50:30 -0300

    ok

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,8 +9,8 @@ export default function Index(){
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"/>
             <div className="container-fluid">
                 <Slider>
-                    <ImageS active="active" img="/images/1.png"/>
-                    <ImageS active="" img="/images/2.png"/>
+                    <ImageS active="" img="/images/1.png"/>
+                    <ImageS active="active" img="/images/2.png"/>
                 </Slider>
             </div>        
         </Layout>
